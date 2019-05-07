f60fc0a70aafd41938065b770cd318e095ff5b66
Jacob Salzberg <jacob@jacobs-laptop.localdomain>
2019-05-07 17:48:46 -0400

    Remove accidentally deleted lines

--- ros.el
+++ ros.el
@@ -1,7 +1,7 @@
 ;; Rahul Org-mode Screenshot takes a screenshot with scrot -s (waits for screen
 ;; selection), saves it as orgfileopened.org_YYYYMMDD_hhmmss.png, inserts 
 ;; the link and turns on the display-inline-images, showing your screenshot directly
-;; to the org-file" 
+;; to the org-file"
 (defun ros ()
   (interactive)
   (let ((filename
@@ -10,6 +10,8 @@
                  "_"
                  (format-time-string "%Y%m%d_%H%M%S")
                  ".png")))
-    (call-process "scrot" nil nil nil "-s" filename)))
+    (call-process "scrot" nil nil nil "-s" filename)
+    (insert (concat "[[" filename "]]"))
+    (org-display-inline-images t t)))
 
 (provide 'ros)

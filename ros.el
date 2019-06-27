a3d7c38ac82ae2fea97d9be9f6214bdcec181abe
Rahul M. Juliato <rmj@macbook.local>
2019-06-27 18:44:50 -0300

    Added macos support

--- ros.el
+++ ros.el
@@ -5,14 +5,28 @@
 (add-hook 'org-mode-hook
 	  (lambda ()
 	  (defun ros ()
-	  (interactive)
-	  (let ((filename
-		 (concat "./"
-			 (file-name-nondirectory buffer-file-name)
-			 "_"
-			 (format-time-string "%Y%m%d_%H%M%S")
-			 ".png")))
-	    (call-process "scrot" nil nil nil "-s" filename)
-	    (insert (concat "[[" filename "]]"))
-	    (org-display-inline-images t t)))))
+	    (interactive)
+	    (if buffer-file-name
+		(progn
+		  (message "Waiting for region selection with mouse...")
+		  (let ((filename
+			 (concat "./"
+				 (file-name-nondirectory buffer-file-name)
+				 "_"
+				 (format-time-string "%Y%m%d_%H%M%S")
+				 ".png")))
+		    (if (executable-find "scrot")
+			(call-process "scrot" nil nil nil "-s" filename)
+		      (call-process "screencapture" nil nil nil "-s" filename))
+		    (insert (concat "[[" filename "]]"))
+		    (org-display-inline-images t t)
+		    )
+		  (message "File created and linked...")
+		  )
+	      (message "You're in a not saved buffer! Save it first!")
+	      )
+	    )
+	  )
+	  )
+
 

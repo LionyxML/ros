3badd43103532ee0b14b666fc9ff3bf7520a0b51
LionyxML <rahul.juliato@gmail.com>
2022-03-03 23:02:47 -0300

    refactor/docs: refactors ros.el to be able to provide M-x ros. Improves documentation with a HOW TO install section

--- ros.el
+++ ros.el
@@ -1,4 +1,4 @@
-;;    ros - Rahul's Org-Mode Screenshot
+;;;;  ros.el - Rahul's Org-Mode Screenshot -*- lexical-binding: t; -*-
 ;;
 ;;    ros is invoked with "M-x ros", then it waits for your selection, takes a
 ;; screenshot and saves it as orgfileopened.org_YYYYMMDD_hhmmss.png.
@@ -8,33 +8,44 @@
 ;; your file.
 ;;
 ;;    Track updates, contact the author and see a demo on github.com/Lionyx/ros
-;; or http://rahuljuliato.com
-;;
-(add-hook 'org-mode-hook
-	  (lambda ()
-	  (defun ros ()
-	    (interactive)
-	    (if buffer-file-name
-		(progn
-		  (message "Waiting for region selection with mouse...")
-		  (let ((filename
-			 (concat "./"
-				 (file-name-nondirectory buffer-file-name)
-				 "_"
-				 (format-time-string "%Y%m%d_%H%M%S")
-				 ".png")))
-		    (if (executable-find "scrot")
-			(call-process "scrot" nil nil nil "-s" filename)
-		      (call-process "screencapture" nil nil nil "-s" filename))
-		    (insert (concat "[[" filename "]]"))
-		    (org-display-inline-images t t)
-		    )
-		  (message "File created and linked...")
-		  )
-	      (message "You're in a not saved buffer! Save it first!")
-	      )
-	    )
-	  )
-	  )
+;;    License: GPL2
 
 
+(defgroup ros nil
+  "Screenshot tool for org-mode."
+  :group 'tools
+  :prefix "ros-*")
+
+(defcustom ros-primary-screencapture "scrot"
+  "Primary screencapture software (usually set to Linux systems)"
+  :type 'string)
+
+(defcustom ros-secondary-screencapture "screencapture"
+  "Secondary screencapture software, if first fails to load (usually set to MacOS systems)"
+  :type 'string)
+
+(defun ros ()
+  "Screenshots an image to an org-file"
+  (interactive)
+  (if buffer-file-name
+      (progn
+        (message "Waiting for region selection with mouse...")
+        (let ((filename
+               (concat "./"
+                       (file-name-nondirectory buffer-file-name)
+                       "_"
+                       (format-time-string "%Y%m%d_%H%M%S")
+                       ".png")))
+          (if (executable-find ros-primary-screencapture)
+              (call-process ros-primary-screencapture nil nil nil "-s" filename)
+            (call-process ros-secondary-screencapture nil nil nil "-s" filename))
+          (insert (concat "[[" filename "]]"))
+          (org-display-inline-images t t)
+          )
+        (message "File created and linked...")
+        )
+    (message "You're in a not saved buffer! Save it first!")
+    )
+  )
+
+(provide 'ros)

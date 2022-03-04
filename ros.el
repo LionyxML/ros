994881718c091feeff1a534050e64a1dd1ee4f6b
LionyxML <rahul.juliato@gmail.com>
2022-03-04 17:37:48 -0300

    fix: fixes package-lint warnings and errors

--- ros.el
+++ ros.el
@@ -1,15 +1,21 @@
 ;;; ros.el --- Rahul's Org-Mode Screenshot -*- lexical-binding: t; -*-
-;;
-;;    ros is invoked with "M-x ros", then it waits for your selection, takes a
+
+;; Author: Rahul Martim Juliato <rahul.juliato@gmail.com>
+;; Version: 0.1.0
+;; URL: https://github.com/LionyxML/ros
+;; Package-Requires: ((emacs "24.1"))
+;; License: GPL2
+
+;;; Commentary:
+;;  ros is invoked with "M-x ros", then it waits for your selection, takes a
 ;; screenshot and saves it as orgfileopened.org_YYYYMMDD_hhmmss.png.
 ;;
-;;    Finally, it keeps working by inserting a link and turning ON the
-;; display-inline-images, showing the taken screenshot directly into
-;; your file.
+;;  It then adds a link to your org file and turns ON display-inline-images
+;; showing you the recently screenshoted image.
 ;;
-;;    Track updates, contact the author and see a demo on github.com/Lionyx/ros
-;;    License: GPL2
+;;  Track updates, contact the author, see contributors and a demo on Github.
 
+;;; Code:
 (require 'org)
 
 (defgroup ros nil
@@ -42,13 +48,9 @@
               (call-process ros-primary-screencapture nil nil nil "-s" filename)
             (call-process ros-secondary-screencapture nil nil nil "-s" filename))
           (insert (concat "[[" filename "]]"))
-          (org-display-inline-images t t)
-          )
-        (message "File created and linked...")
-        )
-    (message "You're in a not saved buffer! Save it first!")
-    )
-  )
+          (org-display-inline-images t t))
+        (message "File created and linked..."))
+    (message "You're in a not saved buffer! Save it first!")))
 
 (provide 'ros)
 
